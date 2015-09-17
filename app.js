647655a9e13de23c3bb1134444571292e7c702d4
asicorsky <andrew.sicorsky@gmail.com>
2015-09-17 15:41:40 +0300

    configure package.json

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ var app = express();
 app.set('view engine', 'ejs');
 app.use('/public', express.static(__dirname + '/public'));
 
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
