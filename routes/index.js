af73fe3d4f2cf28e982f33b63b2f636b928646c6
asicorsky <andrew.sicorsky@gmail.com>
2015-09-17 14:32:23 +0300

    change logs at node.js server

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var phantom = require('phantom');
-var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
